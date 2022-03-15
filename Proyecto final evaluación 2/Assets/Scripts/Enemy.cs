d857717d1ab5334b2e1277763d6dc909825e99b1
Rowen Lumanlan <rowenlumanlan@gmail.com>
2022-03-15 12:50:03 +0100

    a

--- Proyecto final evaluación 2/Assets/Scripts/Enemy.cs
+++ Proyecto final evaluación 2/Assets/Scripts/Enemy.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private Rigidbody enemyRigidbody;
     private GameObject player;
-    [SerializeField] private float speed = 5;
+    [SerializeField] private float speed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyRigidbody = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = (player.transform.position - transform.position).normalized;
-        enemyRigidbody.AddForce(direction * speed);
-
+        transform.LookAt(player.transform.position);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 }
